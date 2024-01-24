14cb4deb9ea501c4ab893ddac783a43762eba90b
Joseph Luyindula <josephluyindula00@gmail.com>
2024-01-25 00:36:28 +0100

    creation du generateur

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PasswordGenerator
-passwords generating program
+### utilités
+Trouver une inspiration pour vos mots de passe, en utilisant **PasswordGenerator** vous serez sur que votre mot de passe est impossible à deviner et voilà vos comptes son un peu plus sûr.
+
+N.B : IL VOUS FAUT ÊTRE PRUDENT AVEC VOS COMPTE.
+
+### installation
+Dans n'importe quel terminal, entez les commandes suivantes :
+
+"""shell
+$ git clone 
+$ cd PasswordGenerator
+$ python terminalpassword.py
+"""
---
+++ terminalpassword.py
@@ -0,0 +1,32 @@
+from string import ascii_letters, digits, punctuation
+from random import randint
+
+# tout les caractères utilisable (crts = caractères)
+crts = ascii_letters+digits+punctuation
+
+#gestion du  nombre de caractère du mot de passe 
+print("Combien de caractère voulez-vous pour voutre mot de passe ? ")
+correct = False
+while correct == False:
+	print("N.B : inscrivez en caractère compris entre 6 et 15 !!! """)
+	try :
+		nb = int(input(">>> "))
+	except :
+		print("Entrez un nombre entier S.V.P")
+	if 6 <= nb <= 15:
+		print("VALIDÉ ☑️")
+		correct = True
+	
+# génération du mot de passe 
+password = ""
+for n in range(nb):
+	 i = randint(0,len(crts))
+	 crt = crts[i]
+	 password += str(crt)
+
+# affichage du mot de passe 
+print(f"""-----------MOT DE PASSE-----------
+
+mot de passe : {password}""")
+
+	 
